0b9a5e361ab28e5ac811616062b02c68b7515725
Sam <sgriffith@alta3.com>
2020-10-27 15:12:35 +0000

    created first challenge

---
+++ pychallenge01.py
@@ -0,0 +1,22 @@
+#Python Challenge Lab # 1
+
+
+#Take these 3 existing lists:
+
+dogs = []
+fish = []
+
+pets = [{"type": "dog", "name": "mudge", "age": 4}, 
+        {"type": "fish", "name": "dorothy", "age": "3 days"},
+		{"type": "dog", "name": "bingo", "age": 12},
+		{"type": "fish", "name": "nemo", "age": 2},
+		{"type": "cat", "name": "fluffy", "age": 1}
+		]
+		
+#Use a for loop to sort each pet dictionary from the 
+#list of pets into the dogs or fish list.
+
+#*Hint: if your iterator is "item" you will need to use item["type"] to
+#       determine what type of animal it is.
+
+#If it is not a dog or a fish, print "Not my pet"
